61f54cb4797bbb1b0f81cf231039b822178b9940
kartikay <kartikaygautam24617@gmail.com>
2024-04-09 20:53:29 +0530

    2

--- src/components/tictactoe.jsx
+++ src/components/tictactoe.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function TicTacToe() {
   return (
     <div>
-      <h1 className="bg-red-600">TicTacToe</h1>
+      <h1 className="bg-red-500">TicTacToe</h1>
     </div>
   );
 }
