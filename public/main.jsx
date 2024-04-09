ed7bad483e15d362f3ca896ea5f11afc59a26aaf
kartikay <kartikaygautam24617@gmail.com>
2024-04-09 19:48:08 +0530

    1

--- public/main.jsx
+++ public/main.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src/app";
 
-ReactDOM.render(<app />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
